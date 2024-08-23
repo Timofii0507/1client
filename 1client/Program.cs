1e1ff4143f7690b64b974c2a8e861e1f1c6fbca2
Юрков Тимофій Олександрович <145855201+Timofii0507@users.noreply.github.com>
2024-08-23 10:17:56 +0300

    hm/ex-1

--- 1client/Program.cs
+++ 1client/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Client
+{
+    static void Main(string[] args)
+    {
+        IPAddress ipAddress = IPAddress.Parse("192.168.0.1");
+        int port = 11000;
+        IPEndPoint remoteEP = new IPEndPoint(ipAddress, port);
+
+        Socket sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+        try
+        {
+            sender.Connect(remoteEP);
+
+            Console.WriteLine($"Connected to {sender.RemoteEndPoint.ToString()}");
+
+            byte[] msg = Encoding.UTF8.GetBytes("Hello, server!");
+            int bytesSent = sender.Send(msg);
+            byte[] bytes = new byte[1024];
+            int bytesRec = sender.Receive(bytes);
+            string data = Encoding.UTF8.GetString(bytes, 0, bytesRec);
+
+            Console.WriteLine($"At {DateTime.Now.ToShortTimeString()} received from {((IPEndPoint)sender.RemoteEndPoint).Address}: {data}");
+
+            sender.Shutdown(SocketShutdown.Both);
+            sender.Close();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+
+        Console.WriteLine("\nPress any key to exit...");
+        Console.ReadKey();
+    }
+}
